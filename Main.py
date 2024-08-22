f1a69b100eee17017c0c75b98763fcb230a18550
NeilSi57 <neil.singh2@education.nsw.gov.au>
2024-08-22 21:18:16 +1000

    Update Main.py
    I added a new function which only shows Australia's inflation rate from 2008-2024, then I updated the bar charts into six groups to display the 2024 inflation rate for all countries.

--- Main.py
+++ Main.py
@@ -10,9 +10,16 @@ quit = False
 original_df =pd.read_csv('data/global_inflation_data.csv')
 
 
-global_inflation_data = pd.read_csv('data/global_inflation_data.csv',
-                            header=None,
-                            names=['Country', 'Inflation_rates', 'Year',])
+global_inflation_data= original_df[['country_name','indicator_name','2008','2012','2016','2020','2024']] 
+
+global_inflation_data = global_inflation_data.dropna()
+
+row1 = global_inflation_data.iloc[0:30]
+row2 = global_inflation_data.iloc[30:60]
+row3 = global_inflation_data.iloc[60:90]
+row4 = global_inflation_data.iloc[90:120]
+row5 = global_inflation_data.iloc[120:150]
+row6 = global_inflation_data.iloc[150:187]
 
 #----Define Functions Below----#
 def showOriginalData():
@@ -22,14 +29,57 @@ def showUpdatedData():
     print(global_inflation_data)
 
 def showCharts():
-    global_inflation_data.plot(
-                    kind='bar',
-                    x='Country',
-                    y='Inflation_rates',
-                    color='blue',
-                    alpha=0.3,
-                    title='Inflation rate per country')
+    row1.plot(
+            kind='bar',
+            x='country_name',
+            y='2024',
+            color='blue',
+            alpha=0.3,
+            title='Inflation rate for 2024')
+    row2.plot(
+            kind='bar',
+            x='country_name',
+            y='2024',
+            color='blue',
+            alpha=0.3,
+            title='Inflation rate for 2024')
+
+    row3.plot(
+            kind='bar',
+            x='country_name',
+            y='2024',
+            color='blue',
+            alpha=0.3,
+            title='Inflation rate for 2024')
+
+    row4.plot(
+            kind='bar',
+            x='country_name',
+            y='2024',
+            color='blue',
+            alpha=0.3,
+            title='Inflation rate for 2024')
+
+    row5.plot(
+            kind='bar',
+            x='country_name',
+            y='2024',
+            color='blue',
+            alpha=0.3,
+            title='Inflation rate for 2024')
+
+    row6.plot(
+            kind='bar',
+            x='country_name',
+            y='2024',
+            color='blue',
+            alpha=0.3,
+            title='Inflation rate for 2024')
     plt.show()
+
+def showAustralia():
+    result_df=global_inflation_data[global_inflation_data['country_name'] == 'Australia']
+    print(result_df)
 
 def userOptions():
     global quit
@@ -53,6 +103,8 @@ def userOptions():
         elif choice == 3:
             showCharts()
         elif choice == 4:
+            showAustralia()
+        elif choice == 5:
             quit = True
         else:
             print('A number between 1 and 4, come on!')
